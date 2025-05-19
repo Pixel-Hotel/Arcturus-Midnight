e59074612de1eaba9fb174e3ccaf27e7651858ca
nextgamingworld <166139327+nextgamingworld@users.noreply.github.com>
2025-05-19 22:47:53 +0200

    test

--- src/main/java/com/eu/habbo/Emulator.java
+++ src/main/java/com/eu/habbo/Emulator.java
@@ -56,7 +56,7 @@ public final class Emulator {
         "██║╚██╔╝██║██║██║  ██║██║╚██╗██║██║██║   ██║██╔══██║   ██║\n" +
         "██║ ╚═╝ ██║██║██████╔╝██║ ╚████║██║╚██████╔╝██║  ██║   ██║\n" +
         "╚═╝     ╚═╝╚═╝╚═════╝ ╚═╝  ╚═══╝╚═╝ ╚═════╝ ╚═╝  ╚═╝   ╚═╝\n" +
-        "Pixel-Hotel.de\n";
+        "Pixel-Hotel.de TEST\n";
 
     public static String build = "";
     public static boolean isReady = false;
