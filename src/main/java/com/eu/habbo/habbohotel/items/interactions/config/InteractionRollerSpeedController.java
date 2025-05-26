6319955e66b1f15935efbe06aa064a04036ef285
Tharwem <ivenhoe90@web.de>
2025-05-26 07:12:11 +0200

    fix roller speed controller

--- src/main/java/com/eu/habbo/habbohotel/items/interactions/config/InteractionRollerSpeedController.java
+++ src/main/java/com/eu/habbo/habbohotel/items/interactions/config/InteractionRollerSpeedController.java
@@ -1,32 +1,26 @@
 package com.eu.habbo.habbohotel.items.interactions.config;
 
+import com.eu.habbo.Emulator;
 import com.eu.habbo.habbohotel.gameclients.GameClient;
 import com.eu.habbo.habbohotel.items.Item;
+import com.eu.habbo.habbohotel.items.interactions.InteractionDefault;
 import com.eu.habbo.habbohotel.items.interactions.InteractionRoller;
 import com.eu.habbo.habbohotel.rooms.Room;
 import com.eu.habbo.habbohotel.rooms.RoomTile;
 import com.eu.habbo.habbohotel.rooms.RoomUnit;
-import com.eu.habbo.habbohotel.users.HabboItem;
-import com.eu.habbo.messages.ServerMessage;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class InteractionRollerSpeedController extends HabboItem {
+public class InteractionRollerSpeedController extends InteractionDefault {
     public InteractionRollerSpeedController(ResultSet set, Item baseItem) throws SQLException {
         super(set, baseItem);
+        setRollerSpeed(Emulator.getGameEnvironment().getRoomManager().getRoom(this.getRoomId()));
     }
 
     public InteractionRollerSpeedController(int id, int userId, Item item, String extradata, int limitedStack, int limitedSells) {
         super(id, userId, item, extradata, limitedStack, limitedSells);
-    }
-
-    @Override
-    public void serializeExtradata(ServerMessage serverMessage) {
-        serverMessage.appendInt((this.isLimited() ? 256 : 0));
-        serverMessage.appendString(this.getExtradata());
-
-        super.serializeExtradata(serverMessage);
+        setRollerSpeed(Emulator.getGameEnvironment().getRoomManager().getRoom(this.getRoomId()));
     }
 
     @Override
@@ -37,8 +31,6 @@ public class InteractionRollerSpeedController extends HabboItem {
     public boolean isWalkable() {
         return false;
     }
-    @Override
-    public void onWalk(RoomUnit roomUnit, Room room, Object[] objects) throws Exception {}
     @Override
     public void onMove(Room room, RoomTile oldLocation, RoomTile newLocation) {
         super.onMove(room, oldLocation, newLocation);
@@ -61,16 +53,16 @@ public class InteractionRollerSpeedController extends HabboItem {
     @Override
     public void onClick(GameClient client, Room room, Object[] objects) throws Exception {
         super.onClick(client, room, objects);
-        setRollerDelay(room);
+        setRollerSpeed(room);
     }
 
     @Override
     public void onPlace(Room room) {
         super.onPlace(room);
-        setRollerDelay(room);
+        setRollerSpeed(room);
     }
 
-    private void setRollerDelay(Room room){
+    private void setRollerSpeed(Room room){
         if(room == null) return;
         room.setRollerSpeed(Integer.parseInt(getExtradata()));
     }
