fb61bf6c3008cac7d96239bcc6bb8589ba3e52ed
Tharwem <ivenhoe90@web.de>
2025-05-27 21:17:37 +0200

    remove debug logs

--- src/main/java/com/eu/habbo/habbohotel/rooms/utils/SpecialItemSet.java
+++ src/main/java/com/eu/habbo/habbohotel/rooms/utils/SpecialItemSet.java
@@ -1,15 +1,11 @@
 package com.eu.habbo.habbohotel.rooms.utils;
 
 import com.eu.habbo.habbohotel.users.HabboItem;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.CopyOnWriteArraySet;
 
 
 public class SpecialItemSet<T extends HabboItem> {
-    private static final Logger LOGGER = LoggerFactory.getLogger(SpecialItemSet.class);
-
     private final CopyOnWriteArraySet<T> items = new CopyOnWriteArraySet<>();
 
     public CopyOnWriteArraySet<T> getItems() {
@@ -27,22 +23,16 @@ public class SpecialItemSet<T extends HabboItem> {
 
     public void add(T item){
         if(isItemNull(item)) return;
-        LOGGER.debug("Add item id: {}", item.getId());
         items.add(item);
     }
 
     public void remove(T item){
         if(isItemNull(item)) return;
-        LOGGER.debug("Removed item id: {}", item.getId());
         items.remove(item);
     }
 
     private boolean isItemNull(T item){
-        if(item == null){
-            LOGGER.warn("Item was null!");
-            return true;
-        }
-        return false;
+        return item == null;
     }
     public void dispose(){
         items.clear();
