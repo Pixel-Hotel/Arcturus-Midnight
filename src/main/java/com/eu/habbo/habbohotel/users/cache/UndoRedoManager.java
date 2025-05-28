925228da9e47ca104edf7771b42e69e62b8868a0
Tharwem <ivenhoe90@web.de>
2025-05-28 18:33:48 +0200

    try to implement undo and redo system

--- src/main/java/com/eu/habbo/habbohotel/users/cache/UndoRedoManager.java
+++ src/main/java/com/eu/habbo/habbohotel/users/cache/UndoRedoManager.java
@@ -2,11 +2,14 @@ package com.eu.habbo.habbohotel.users.cache;
 
 import com.eu.habbo.habbohotel.users.cache.actions.ItemAction;
 import com.eu.habbo.habbohotel.users.cache.actions.ItemActionKey;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.*;
 
 public class UndoRedoManager {
 
+    public static final Logger LOGGER = LoggerFactory.getLogger(UndoRedoManager.class);
     public static final int MAX_STEPS = 10;
     private final Deque<ItemAction> undoStack = new ArrayDeque<>();
     private final Deque<ItemAction> redoStack = new ArrayDeque<>();
@@ -42,12 +45,14 @@ public class UndoRedoManager {
 
         Deque<ItemAction> fromStack = undo ? undoStack : redoStack;
         Deque<ItemAction> toStack = undo ? redoStack: undoStack;
-
+        LOGGER.debug("{}: fromStack: {} -> toStack: {}", undo ? "UNDO" : "REDO", fromStack, toStack);
         for(int i = 0; i < steps; i++) {
             ItemAction command = fromStack.pop();
 
             ItemActionKey key = new ItemActionKey(command.getItem().getId(), command.getClass());
+            LOGGER.debug("{} step {}: {} - {}",undo ? "Undoing" : "Redoing", i, command.getItem().getId(), command.getClass().getSimpleName());
             uniqueActions.put(key, command);
+            LOGGER.debug("Unique actions: {}", uniqueActions.size());
             pushLimited(toStack, command);
         }
 
